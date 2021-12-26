6c5d86de5ff135773e5fd157aec718e76348b4aa
Sean Luo <41530060+lu0x1a0@users.noreply.github.com>
2021-12-26 20:00:48 +1100

    added initial model to test

---
+++ Models/neural.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import numpy as np
+#import random
+import time
+#from scipy.stats import multivariate_normal
+#from scipy.stats import norm
+#import math
+#import os
+class STDConvModel(nn.Module):
+    def __init__(self,input_length,num_features,rolling_size,stride1=1,stride2=1):
+        super(STDConvModel, self).__init__()
+        self.num_features = num_features
+        self.conv1d = nn.Conv1d(
+            in_channels = num_features, 
+            out_channels = num_features+1, 
+            kernel_size = rolling_size,
+            stride = stride1 
+        )
+        # output = (batch,out_channel, int(1+(window_size-rolling_size)/stride) )
+
+        self.conv1_2_reshape = lambda x: torch.unsqueeze(x,1)
+        self.conv2d_out_channel = 2
+        self.conv2d = nn.Conv2d(
+            1,
+            self.conv2d_out_channel,
+            kernel_size = (num_features+1,rolling_size),
+            stride = stride2
+        )
+        
+        l1_out = int((input_length-rolling_size)/stride1+1)
+        l2_out = int((l1_out-rolling_size)/stride2+1)
+        # output
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(self.conv2d_out_channel*1*l2_out,int(l2_out/2)),
+            nn.Sigmoid(),
+            nn.Linear(int(l2_out/2),1),
+            nn.Tanh()
+        )
+    def forward(self,x):
+        #out1 = nn.ReLU()(self.conv1d(x))
+        out1 = nn.BatchNorm1d(self.num_features+1)(nn.ReLU()(self.conv1d(x)))
+        out2 = self.conv1_2_reshape(out1)
+        #out3 = nn.ReLU()(self.conv2d(out2))
+        out3 = nn.BatchNorm2d(self.conv2d_out_channel)(nn.ReLU()(self.conv2d(out2)))
+        out4 = self.fc(out3)
+        return out4
+from torchinfo import summary
+m1 = STDConvModel(5*24, 2, 12,stride1=2,stride2=2)
+print(m1(torch.zeros((20,2,5*24))))
+class STDLSTM(nn.Module):
+    pass
+class FC(nn.Module):
+    def __init__(self,layer_shapes,activation = nn.Sigmoid, out_act = nn.Sigmoid):
+        super().__init__()
+        self.topology = layer_shapes
+        self.layerslist = []
+        for i,x in enumerate(self.topology):
+            if i != (len(self.topology)-1):
+                self.layerslist.append(nn.Linear(*x))
+                self.layerslist.append(activation())
+            else:
+                self.layerslist.append(nn.Linear(*x))
+                self.layerslist.append(out_act())
+        self.nn = nn.Sequential(*self.layerslist)
+
+    def forward(self,x):
+        return self.nn(x)
+    def encode(self):
+        return torch.cat([x.flatten() for x in self.parameters()])
+    def decode(self,w):
+        cumidx = 0
+        for p in self.parameters():
+            nneurons = torch.numel(p)
+            p.data = w[cumidx:cumidx+nneurons].reshape(p.data.shape)
+            cumidx += nneurons
